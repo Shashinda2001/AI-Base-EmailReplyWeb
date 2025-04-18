bb47db0636068004c6965f984e56e34eadac4682
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2025-04-18 15:25:23 +0530

    Configure WebClient and prepare request body for Gemini API integration

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -1,14 +1,38 @@
 package com.email.writer.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class EmailGeneratorService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public EmailGeneratorService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
     public String generateEmailReply(EmailRequest emailRequest){
         //Build the prompt
         String prompt  = buildPrompt(emailRequest);
         //craft request
+        Map<String,Object> requestBody=  Map.of(
+                "contents", new Object[]{
+                        Map.of("parts",new Object[]{
+                                Map.of("text",prompt)
+                        })
+        }
+        );
         //do request and get response
+        
         //return response
         return "none";
     }
