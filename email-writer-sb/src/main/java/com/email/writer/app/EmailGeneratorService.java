8834a92eaf8c716453254d2cde23debef3558869
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2025-04-18 14:19:09 +0530

    Initial commit

---
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -0,0 +1,14 @@
+package com.email.writer.app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailGeneratorService {
+    public String generateEmailReply(EmailRequest emailRequest){
+        //Build the prompt
+        //craft request
+        //do request and get response
+        //return response
+        return "none";
+    }
+}
