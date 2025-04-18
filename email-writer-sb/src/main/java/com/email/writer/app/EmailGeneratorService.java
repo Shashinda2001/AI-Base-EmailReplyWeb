d9af83690069526cc253be919206496e1c14f1c3
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2025-04-18 14:43:13 +0530

    Add method to build prompt for generating professional email replies with optional tone

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -6,9 +6,21 @@ import org.springframework.stereotype.Service;
 public class EmailGeneratorService {
     public String generateEmailReply(EmailRequest emailRequest){
         //Build the prompt
+        String prompt  = buildPrompt(emailRequest);
         //craft request
         //do request and get response
         //return response
         return "none";
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply the following email content. Please don't generate a subject line ");
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone. ");
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+
+        return  prompt.toString();
     }
 }
